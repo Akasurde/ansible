f8bcb971b711dec5b739d977ee5f43a8bf5ce409
Anthony Bond <ajbond2005@gmail.com>
2017-11-02 06:07:57 -0400

    Resolved bad responses and cleaned up code
    Updated docs and other common DO module variable names for oauth_token

--- lib/ansible/modules/cloud/digital_ocean/digital_ocean_domain.py
+++ lib/ansible/modules/cloud/digital_ocean/digital_ocean_domain.py
@@ -27,7 +27,7 @@ options:
      - Indicate desired state of the target.
     default: present
     choices: ['present', 'absent']
-  api_token:
+  oauth_token:
     description:
      - DigitalOcean api token.
     version_added: "1.9.5"
@@ -42,8 +42,8 @@ options:
      - The IP address to point a domain at.
 
 notes:
-  - Two environment variables can be used, DO_API_KEY and DO_API_TOKEN. They both refer to the v2 token.
-  - As of Ansible 1.9.5 and 2.0, Version 2 of the DigitalOcean API is used, this removes C(client_id) and C(api_key) options in favor of C(api_token).
+  - Environment variable DO_OAUTH_TOKEN can be used for the oauth_token.
+  - As of Ansible 1.9.5 and 2.0, Version 2 of the DigitalOcean API is used, this removes C(client_id) and C(api_key) options in favor of C(oauth_token).
   - If you are running Ansible 1.9.4 or earlier you might not be able to use the included version of this module as the API version used has been retired.
 
 requirements:
@@ -82,6 +82,7 @@ import traceback
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.digital_ocean import DigitalOceanHelper
 from ansible.module_utils._text import to_native
+from ansible.module_utils.basic import env_fallback
 
 
 class DoManager(DigitalOceanHelper, object):
@@ -91,21 +92,22 @@ class DoManager(DigitalOceanHelper, object):
         self.domain_ip = module.params.get('ip', None)
         self.domain_id = module.params.get('id', None)
 
+    @staticmethod
+    def jsonify(response):
+        return response.status_code, response.json
+
     def all_domains(self):
         resp = self.get('domains/')
-        return resp['domains']
+        return resp
 
     def find(self):
-        if self.domain_name is None or self.domain_id is None:
+        if self.domain_name is None and self.domain_id is None:
             return False
-
         domains = self.all_domains()
-        for domain in domains:
-            if domain.id == self.domain_id:
-                return domain
-            elif domain.name == self.domain_name:
-                return domain
-
+        status, json = self.jsonify(domains)
+        for domain in json['domains']:
+            if domain['name'] == self.domain_name:
+                return True
         return False
 
     def add(self):
@@ -119,54 +121,72 @@ class DoManager(DigitalOceanHelper, object):
             return json
 
     def all_domain_records(self):
-        resp = self.get('domains/%s/records/' % self.domain_id)
-        return resp['domain_records']
+        resp = self.get('domains/%s/records/' % self.domain_name)
+        return resp.json
+
+    def domain_record(self):
+        resp = self.get('domains/%s' % self.domain_name)
+        status, json = self.jsonify(resp)
+        return json
 
     def destroy_domain(self):
-        self.delete('domains/%s' % self.domain_id)
-        return True
+        resp = self.delete('domains/%s' % self.domain_name)
+        status, json = self.jsonify(resp)
+        if status == 204:
+            return True
+        else:
+            return json
 
     def edit_domain_record(self):
         params = {'name': self.domain_name}
-        resp = self.put('domains/%s/records/%s' % (self.domain_id, self.domain_ip), data=params)
+        resp = self.put('domains/%s/records/%s' % (self.domain_name, self.domain_id), data=params)
         return resp['domain_record']
 
 
 def core(module):
-    do_manger = DoManager(module)
+    do_manager = DoManager(module)
     state = module.params.get('state')
 
-    domain = do_manger.find()
+    domain = do_manager.find()
     if state == 'present':
         if not domain:
-            domain = do_manger.add()
+            domain = do_manager.add()
             if 'message' in domain:
                 module.fail_json(changed=False, msg=domain['message'])
             else:
                 module.exit_json(changed=True, domain=domain)
         else:
-            records = do_manger.all_domain_records()
+            records = do_manager.all_domain_records()
             at_record = None
-            for record in records:
-                if record.name == "@" and record.type == 'A':
+            for record in records['domain_records']:
+                if record['name'] == "@" and record['type'] == 'A':
                     at_record = record
 
-            if not at_record.data == module.params.get('ip'):
-                do_manger.edit_domain_record()
-                module.exit_json(changed=True, domain=do_manger.find())
+            if not at_record['data'] == module.params.get('ip'):
+                do_manager.edit_domain_record()
+                module.exit_json(changed=True, domain=do_manager.find())
+            else:
+                module.exit_json(changed=False, domain=do_manager.domain_record())
 
     elif state == 'absent':
         if not domain:
-            module.fail_json(changed=False, msg="Domain not found.")
-        delete_event = do_manger.destroy_domain()
-        module.exit_json(changed=delete_event)
+            module.exit_json(changed=False, msg="Domain not found")
+        else:
+            delete_event = do_manager.destroy_domain()
+            if not delete_event:
+                module.fail_json(changed=False, msg=delete_event['message'])
+            else:
+                module.exit_json(changed=True, event=None)
 
 
 def main():
     module = AnsibleModule(
         argument_spec=dict(
             state=dict(choices=['present', 'absent'], default='present'),
-            api_token=dict(aliases=['API_TOKEN'], no_log=True),
+            oauth_token=dict(
+                aliases=['API_TOKEN'],
+                no_log=True,
+                fallback=(env_fallback, ['DO_API_TOKEN', 'DO_API_KEY', 'DO_OAUTH_TOKEN'])),
             name=dict(type='str'),
             id=dict(aliases=['droplet_id'], type='int'),
             ip=dict(type='str'),
